f24e3ea310a0d9b877168422946df90ac7b21a46
knmarcin <knapmarcin@icloud.com>
2021-12-21 13:25:00 +0100

    code format

--- tracker/views.py
+++ tracker/views.py
@@ -1,24 +1,26 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
-from rest_framework import status, generics
+from rest_framework import status
 from rest_framework.response import Response
 
-from tracker.models import Habit, Counter
+from tracker.models import Habit
 from tracker.serializers import HabitSerializer, HabitPostSerializer
 
+
 class HabitViewSet(APIView):
-    def get(self, request):
+    @staticmethod
+    def get(request):
         try:
             queryset = Habit.objects.filter(owner_id=request.user)
             serializer = HabitSerializer(queryset, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
-        except (TypeError):
+        except TypeError:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
-    def post(self, request):
+
+    @staticmethod
+    def post(request):
         serializer = HabitPostSerializer(data=request.data, context={'request': request})
         if serializer.is_valid():
             name = serializer.validated_data['name']
-
             new_object = Habit.objects.create(name=name, owner=request.user)
             new_object.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
