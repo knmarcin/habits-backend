a934d2559f975328f18eac42f03c2a6b58efdb30
knmarcin <knapmarcin@icloud.com>
2021-12-21 13:23:39 +0100

    remove pagination for now

--- habits/settings.py
+++ habits/settings.py
@@ -124,8 +124,3 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# REST_FRAMEWORK
-REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20
-}
