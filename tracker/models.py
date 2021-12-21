7867f3751dec20fc9fb3821b56882bd007c3e2a6
knmarcin <knapmarcin@icloud.com>
2021-12-21 11:23:27 +0100

    add prototype model for habit, counter

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Habit(models.Model):
+    '''Simple model for Habit, soon to be added FK to user.'''
+    name = models.CharField(max_length=30)
+    created_at = models.DateField(auto_now_add=True)
+
+
+class Counter(models.Model):
+    '''Simple counters for habits with unique date/habit_id'''
+    accomplished = models.DateField(auto_now_add=True)
+    habit = models.ForeignKey(Habit, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('accomplished', 'habit',)
